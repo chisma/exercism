99b9adb3f89aecb75dc784eb177b5a74f010f1ea
chisma <soumya.saligram@gmail.com>
2016-10-16 20:01:46 +0200

    version 2 - rna transcription

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,14 +1,11 @@
-def to_rna(sequence):
-    """Takes a string representative of DNA nucleotide/s
-    Returns a string representative of corresponding RNA nucleotide"""
-    RNA_map = []
-    for ch in sequence:
-        if ch is 'C':
-            RNA_map.append('G')
-        elif ch is 'G':
-            RNA_map.append('C')
-        elif ch is 'T':
-            RNA_map.append('A')
-        else:
-            RNA_map.append('U')
-    return "".join(RNA_map)
+def transform(key):
+    return {
+        'C': 'G',
+        'G': 'C',
+        'T': 'A',
+        'A': 'U'
+    }[key]
+
+
+def to_rna(seq):
+    return ''.join(map(transform, seq))
