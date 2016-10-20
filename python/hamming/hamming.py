3059ea73e7978072ee70e51ce4b947da7784d796
chisma <soumya.saligram@gmail.com>
2016-10-20 09:52:52 +0200

    another flake8 modification

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,6 +1,8 @@
+import operator
+
+
 def distance(strand1, strand2):
     """Takes two DNA strands and computes hamming distance
     Returns the hamming distance as integer"""
-    import operator
     return map(operator.eq, strand1, strand2).count(False)
 #   return map(lambda (x,y):x == y, zip(strand1,strand2)).count(False)
