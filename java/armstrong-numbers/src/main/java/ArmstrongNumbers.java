49856fe6226fc4b1e1adc706c174b2062b050727
Soumya Prasad <snp@blandings.lan>
2019-02-10 13:47:37 +0100

    Adding gitignore file!

--- java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -1,4 +1,3 @@
-import java.math.BigInteger;
 import java.util.Arrays;
 
 class ArmstrongNumbers {
@@ -7,7 +6,6 @@ class ArmstrongNumbers {
 
 
         String numberAsString = String.valueOf(numberToCheck);
-        System.out.println(numberAsString);
         String[] array = numberAsString.split("");
         int length = numberAsString.length();
         int sum = 0;
@@ -21,24 +19,3 @@ class ArmstrongNumbers {
 
     }
 }
-
-        /*Arrays.stream(array).forEach(digit -> {
-
-             sum += (int)Math.pow(Integer.valueOf(digit), length));
-            if (sum == numberToCheck || length == 1) {
-                return true;
-            } else
-                return false;
-
-        });*/
-
-
-//        System.out.println(length);
-//        int sum = 0;
-//        for (int i = 0; i < length; i++) {
-//            sum += Math.pow(Integer.valueOf(array[i]), length);
-//            System.out.println(sum);
-//
-//        }
-
-
